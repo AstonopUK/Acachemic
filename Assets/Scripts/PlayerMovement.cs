c704824182097e618a0a537e1802439ac99e2de4
astonopuk <ben.hopgood@mail.bcu.ac.uk>
2020-02-04 15:18:32 +0000

    Menu

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public SteamVR_Action_Vector2 moveInput;
+    public float moveSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position += moveSpeed * Time.smoothDeltaTime * new Vector3(moveInput.axis.x, 0, moveInput.axis.y);
+
+    }
+
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public int LevelSelect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        
+
+    }
+
+    public void LoadScene()
+    {
+
+        switch (LevelSelect)
+        {
+
+            case 0:
+                Application.Quit();
+                return;
+
+            case 1:
+                SceneManager.LoadScene("MainMenu");
+                return;
+
+            case 2:
+                SceneManager.LoadScene("Tutorial");
+                return;
+
+            case 3:
+
+                return;
+
+            default:
+                return;
+
+        }
+
+    }
+
+}
