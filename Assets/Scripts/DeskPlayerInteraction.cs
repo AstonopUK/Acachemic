572dd7746b5a8f455da3f3d70b09f2c0b7399e89
astonop <ben_hopgood@yahoo.co.uk>
2020-04-18 19:19:37 +0100

    Big additions

---
+++ Assets/Scripts/DeskPlayerInteraction.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeskPlayerInteraction : MonoBehaviour
+{
+
+    public float interactRange = 0.001f;
+    public bool isInteract;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(gameObject.transform.position, gameObject.transform.forward, out hit, interactRange))
+            {
+
+                isInteract = (hit.collider.tag == "Clickable");
+                Debug.Log("Interacted with " + hit.collider.name);
+
+            }
+
+        }
+
+    }
+
+}
